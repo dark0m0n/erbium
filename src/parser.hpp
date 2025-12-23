1418770d4f0903758e2f9b5e017beb392a9f5b70
dark0m0n <ana1lphaon@gmail.com>
2025-12-23 12:50:36 +0200

    create Parser class for parsing tokens into nodes

---
+++ src/parser.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace node {
+    struct Expr {
+        Token token;
+    };
+
+    struct Exit {
+        Expr expr;
+    };
+}
+
+class Parser {
+public:
+    explicit Parser(const std::vector<Token> &tokens)
+            : tokens(tokens) {
+    }
+
+    std::optional<node::Expr> parse_expr() {
+        if (peak().has_value() && peak()->type == TokenType::int_literal) {
+            return node::Expr(consume());
+        }
+        return std::nullopt;
+    }
+
+    std::optional<node::Exit> parse() {
+        std::optional<node::Exit> exit_node = std::nullopt;
+        while (peak().has_value()) {
+            if (peak()->type == TokenType::exit) {
+                consume();
+                if (auto node_expr = parse_expr()) {
+                    exit_node = node::Exit(*node_expr);
+                } else {
+                    std::cerr << "Invalid expression" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                if (peak().has_value() && peak()->type == TokenType::semicolon) {
+                    consume();
+                } else {
+                    std::cerr << "Expected ';'" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+        index = 0;
+        return exit_node;
+    }
+
+private:
+    const std::vector<Token> tokens;
+    size_t index = 0;
+
+    [[nodiscard]] std::optional<Token> peak(const int ahead = 1) const {
+        if (index + ahead > tokens.size()) {
+            return std::nullopt;
+        }
+        return tokens[index];
+    }
+
+    Token consume() {
+        return tokens[index++];
+    }
+};
